369b88f6e93c3667add6ac1be6b41306ebd26624
Blythe Totsch <uncblythe1@gmail.com>
2018-08-23 12:01:09 -0500

    Completed Project

--- page1.js
+++ page1.js
@@ -1,63 +1,201 @@
 document.addEventListener('DOMContentLoaded', (ev) => {
+
+
+let selected;
+
+let basicTimeline = anime.timeline();
+
+basicTimeline
+  .add({
+    targets: '#flappybird',
+    translateY: [0, 500],
+    translateX: [0, 100],
+    loop: false,
+    duration: 2000,
+    rotate: '1turn',
+    easing: 'easeInOutQuad',
+  })
+  .add({
+    targets: '#flappybird2',
+    translateX: [-5, 400],
+    translateY: 40,
+    opacity: [0, 100],
+    scale: 1.5,
+    loop: false,
+    duration: 3000,
+    easing: 'easeInOutQuad',
+  });
   
+
 let song1 = document.getElementsByClassName('song1')[0];
 song1.innerText = 'Wrecking Ball';
 song1.addEventListener('click', (ev) => {
-    audio.src = 'assets/test.mp3';
+    selected = 'assets/test.mp3';
+    anime ({
+        targets: '.song1',
+        translateX: 36.9,
+        translateY: 20,
+        scale: 2.5,
+        opacity: 1,
+        rotate: '2turn',
+    })
+    anime ({
+        targets: '.chooseSong',
+        opacity: 0,
+    })
 })
 
 let song2 = document.getElementsByClassName('song2')[0];
 song2.innerText = 'She Wolf';
 song2.addEventListener('click', (ev) => {
-    audio.src = 'assets/test.mp3';
+    selected = 'assets/David Guetta - She Wolf ft. Sia.mp3';  
+    anime ({
+        targets: '.song2',
+        translateX: 30,
+        translateY: 20,
+        scale: 2.5,
+        rotate: '2turn',
+    })
+    anime ({
+        targets: '.chooseSong',
+        opacity: 0,
+    })  
 })
 
 let song3 = document.getElementsByClassName('song3')[0];
 song3.innerText = 'The Monster';
 song3.addEventListener('click', (ev) => {
-    audio.src = 'assets/test.mp3';
+    selected = 'assets/12 The Monster (feat. Rihanna).mp3';
+    anime ({
+        targets: '.song3',
+        translateX: 24,
+        translateY: 20,
+        scale: 2.5,
+        rotate: '2turn',
+    })
+    anime ({
+        targets: '.chooseSong',
+        opacity: 0,
+    })  
 })
 
 let song4 = document.getElementsByClassName('song4')[0];
 song4.innerText = 'Lean On';
 song4.addEventListener('click', (ev) => {
-    audio.src = 'assets/test.mp3';
+    selected = 'assets/01 Lean On.mp3';
+    anime ({
+        targets: '.song4',
+        translateX: 17,
+        translateY: 20,
+        scale: 2.5,
+        rotate: '2turn',
+    })
+    anime ({
+        targets: '.chooseSong',
+        opacity: 0,
+    })  
 })
 
 let song5 = document.getElementsByClassName('song5')[0];
 song5.innerText = 'Young Blood';
 song5.addEventListener('click', (ev) => {
-    audio.src = 'assets/test.mp3';
+    selected = 'assets/01 Young Blood.m4a';
+    anime ({
+        targets: '.song5',
+        translateX: 10,
+        translateY: 20,
+        scale: 2.5,
+        rotate: '2turn',
+    })
+    anime ({
+        targets: '.chooseSong',
+        opacity: 0,
+    })  
 })
 
 let song6 = document.getElementsByClassName('song6')[0];
 song6.innerText = 'Work From Home';
 song6.addEventListener('click', (ev) => {
-    audio.src = 'assets/test.mp3';
+    selected = 'assets/Fifth Harmony - Work from Home ft. Ty Dolla $ign.mp3';
+    anime ({
+        targets: '.song6',
+        translateX: 4,
+        translateY: 20,
+        scale: 2.5,
+        rotate: '2turn',
+    })
+    anime ({
+        targets: '.chooseSong',
+        opacity: 0,
+    })  
 })
 
 let song7 = document.getElementsByClassName('song7')[0];
 song7.innerText = `Don't Let Me Down`;
 song7.addEventListener('click', (ev) => {
-    audio.src = 'assets/test.mp3';
+    selected = `assets/The Chainsmokers - Don't Let Me Down (Audio) ft. Daya.mp3`;
+    anime ({
+        targets: '.song7',
+        translateX: -4,
+        translateY: 20,
+        scale: 2.5,
+        rotate: '2turn',
+    })
+    anime ({
+        targets: '.chooseSong',
+        opacity: 0,
+    })  
 })
 
 let song8 = document.getElementsByClassName('song8')[0];
 song8.innerText = 'Chandelier';
 song8.addEventListener('click', (ev) => {
-    audio.src = 'assets/test.mp3';
+    selected = 'assets/Chandelier.mp3';
+    anime ({
+        targets: '.song8',
+        translateX: -10,
+        translateY: 20,
+        scale: 2.5,
+        rotate: '2turn',
+    })
+    anime ({
+        targets: '.chooseSong',
+        opacity: 0,
+    })  
 })
 
 let song9 = document.getElementsByClassName('song9')[0];
 song9.innerText = 'Head Full of Dreams';
 song9.addEventListener('click', (ev) => {
-    audio.src = 'assets/test.mp3';
+    selected = 'assets/11 Up & Up.mp3';
+    anime ({
+        targets: '.song9',
+        translateX: -15,
+        translateY: 20,
+        scale: 2.5,
+        rotate: '2turn',
+    })
+    anime ({
+        targets: '.chooseSong',
+        opacity: 0,
+    })  
 })
 
 let song10 = document.getElementsByClassName('song10')[0];
 song10.innerText = '1989';
 song10.addEventListener('click', (ev) => {
-    audio.src = 'assets/test.mp3';
+    selected = 'assets/02 Blank Space.mp3';
+    anime ({
+        targets: '.song10',
+        translateX: -22,
+        translateY: 20,
+        scale: 2.5,
+        rotate: '2turn',
+    })
+    anime ({
+        targets: '.chooseSong',
+        opacity: 0,
+    })  
 })
 
     let animateTitle = anime({
@@ -67,7 +205,17 @@ song10.addEventListener('click', (ev) => {
         opacity: 50,
         loop: 1,
         fontSize: 75,
-        delay: 100,
+        delay: 200,
+        duration: 1000
+    })
+
+    let animateChooseSong = anime({
+        targets: '.chooseSong',
+        translateY: '19rem',
+        translateX: '37rem',
+        scale: [1, 3],
+        opacity: [0, .8],
+        delay: 500,
         duration: 1000
     })
 
@@ -78,7 +226,7 @@ song10.addEventListener('click', (ev) => {
         opacity: [0, .8],
         scale: [1, 1.6],
         borderRadius: ['0em', '5em'],
-        delay: 200,
+        delay: 500,
         direction: 'alternate',
         loop: false,
         duration: 1000
@@ -91,7 +239,7 @@ song10.addEventListener('click', (ev) => {
         opacity: [0, .8],
         scale: [1, 1.6],
         borderRadius: ['0em', '5em'],
-        delay: 300,
+        delay: 700,
         direction: 'alternate',
         loop: false,
         duration: 1000
@@ -104,7 +252,7 @@ song10.addEventListener('click', (ev) => {
         opacity: [0, .8],
         scale: [1, 1.6],
         borderRadius: ['0em', '5em'],
-        delay: 400,
+        delay: 900,
         direction: 'alternate',
         loop: false,
         duration: 1000
@@ -117,7 +265,7 @@ song10.addEventListener('click', (ev) => {
         opacity: [0, .8],
         scale: [1, 1.6],
         borderRadius: ['0em', '5em'],
-        delay: 500,
+        delay: 1100,
         direction: 'alternate',
         loop: false,
         duration: 1000
@@ -130,7 +278,7 @@ song10.addEventListener('click', (ev) => {
         opacity: [0, .8],
         scale: [1, 1.6],
         borderRadius: ['0em', '5em'],
-        delay: 600,
+        delay: 1300,
         direction: 'alternate',
         loop: false,
         duration: 1000
@@ -143,7 +291,7 @@ song10.addEventListener('click', (ev) => {
         opacity: [0, .8],
         scale: [1, 1.6],
         borderRadius: ['0em', '5em'],
-        delay: 700,
+        delay: 1300,
         direction: 'alternate',
         loop: false,
         duration: 1000
@@ -156,7 +304,7 @@ song10.addEventListener('click', (ev) => {
         opacity: [0, .8],
         scale: [1, 1.6],
         borderRadius: ['0em', '5em'],
-        delay: 800,
+        delay: 1100,
         direction: 'alternate',
         loop: false,
         duration: 1000
@@ -182,7 +330,7 @@ song10.addEventListener('click', (ev) => {
         opacity: [0, .8],
         scale: [1, 1.6],
         borderRadius: ['0em', '5em'],
-        delay: 1000,
+        delay: 700,
         direction: 'alternate',
         loop: false,
         duration: 1000
@@ -195,16 +343,25 @@ song10.addEventListener('click', (ev) => {
         opacity: [0, .8],
         scale: [1, 1.6],
         borderRadius: ['0em', '5em'],
-        delay: 1100,
+        delay: 500,
         direction: 'alternate',
         loop: false,
         duration: 1000
     })
 
+    let animateInstructions = anime ({
+        targets: '#instructions',
+        translateY : '10rem',
+        translateX : '-5rem',
+        opacity: [0, .9],
+        delay: 1100,
+        duration: 1000,
+    })
+
     let animatePlaySong = anime ({
         targets: '#playSong',
-        translateY : '22rem',
-        translateX : '-5rem',
+        translateY : '25rem',
+        translateX : '5rem',
         opacity: [0, .9],
         delay: 1100,
         duration: 1000,
@@ -212,22 +369,63 @@ song10.addEventListener('click', (ev) => {
 
     let animatePlayGame = anime ({
         targets: '#playGame',
-        translateY : '35rem',
-        translateX : '-4rem',
+        translateY : '30rem',
+        translateX : '8rem',
         opacity: [0, .9],
         delay: 1100,
         duration: 1000,
 
     })
 
+    let instructions = document.getElementById('instructions');
+    instructions.addEventListener('click', (ev) => {
+        instructions.innerText = '';
+        let instructionList = document.createElement('ul');
+        instructionList.innerText = 'INSTRUCTIONS:'
+        instructions.appendChild(instructionList);
+
+        let liOne = document.createElement('li');
+        instructionList.appendChild(liOne);
+        liOne.innerText = "Use the spacebar to fly."
+
+        let liTwo = document.createElement('li');
+        instructionList.appendChild(liTwo);
+        liTwo.innerText = "Collect the pink bugs for your babies."
+
+        let liThree = document.createElement('li');
+        instructionList.appendChild(liThree);
+        liThree.innerText = "Be aware of the poisonous yellow bugs - they will kill you."
+
+        anime ({
+            targets: '.chooseSong',
+            opacity: 0,
+        })
+        anime ({
+            targets: 'h1',
+            opacity: 0,
+        })
+        anime ({
+            targets: '.song',
+            opacity: 0,
+        })
+        anime ({
+            targets: '#instructions',
+            scale: [1, 4],
+            translateY : '5rem',
+            translateX : '-10rem',
+        })
+        
+    })
+
     let playSong = document.getElementById('playSong');
     playSong.addEventListener('click', (ev) => {
-        window.open('page3.html');
+        window.open(`page3.html?song=${selected}`);
     })
 
     let playGame = document.getElementById('playGame');
     playGame.addEventListener('click', (ev) => {
-        window.open('page2.html')
+        window.open(`page2.html?song=${selected}`);
     })
 
 })
+
--- page2.js
+++ page2.js
@@ -1,218 +1,173 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-	let X = window.innerWidth;
-	let Y = window.innerHeight;
-
-	let windowX = Math.ceil(X / 1.5);
-	let windowY = Math.ceil(Y / 1.5) - 30;
-	let gm = document.getElementById('analyser_render');
-	console.log(gm);
-	gm.width = X / 1.5;
-	gm.height = Y / 3 - 20;
-	console.log(gm.width, gm.height);
-
-	let mainState = {
-		preload: function () {
-			// This function will be executed at the beginning     
-			// That's where we load the images and sounds 
-			game.load.image('bird', 'assets/new_bird1.png');
-			game.load.image('goodbug', 'assets/good_bug.png');
-			game.load.image('badbug', 'assets/badbug.png');
-			//game.load.image('fud', 'assets/diamond.png');
-		},
-
-		create: function () {
-			// This function is called after the preload function     
-			// Here we set up the game, display sprites, etc.  
-			// Change the background color of the game to blue
-			game.stage.backgroundColor = '#002D3C';
-
-			// Set the physics system
-			game.physics.startSystem(Phaser.Physics.ARCADE);
-
-			// Display the bird at the position x=100 and y=245
-			this.bird = game.add.sprite(10, windowY / 2, 'bird');
-			this.bird.scale.setTo(0.1, 0.1);
-			// Add physics to the bird
-			// Needed for: movements, gravity, collisions, etc.
-			game.physics.arcade.enable(this.bird);
-
-			// Add gravity to the bird to make it fall
-			this.bird.body.gravity.y = windowY / 2;
-
-			// add the pipes group
-			this.pipes = game.add.group();
-
-			this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
-
-			// add food to the game
-			this.food = game.add.group();
-			this.timer = game.time.events.loop(1000, this.addfood, this);
-
-
-			// add the score to the top left 
-			this.score = 0;
-			this.labelScore = game.add.text(20, 20, "0",
-				{ font: "30px Arial", fill: "#ffffff" });
-
-			// Call the 'jump' function when the spacekey is hit
-			let spaceKey = game.input.keyboard.addKey(
-				Phaser.Keyboard.SPACEBAR);
-			spaceKey.onDown.add(this.jump, this);
-		},
-
-		update: function () {
-			// This function is called 60 times per second    
-			// It contains the game's logic   
-			if (this.bird.y < 0 )
-				this.restartGame();
-			game.physics.arcade.overlap(
-				this.bird, this.food, this.eatFood, null, this);
-			game.physics.arcade.overlap(
-				this.bird, this.pipes, this.restartGame, null, this);
-		},
-		// Make the bird jump 
-		jump: function () {
-			// Add a vertical velocity to the bird
-			this.bird.body.velocity.y = -windowY / 2;
-		},
-
-		// add a pipe
-		addOnePipe: function (x, y) {
-			// Create a pipe at the position x and y
-			let badbug = game.add.sprite(x, y, 'badbug');
-			badbug.scale.setTo(0.8, 0.8);
-			// Add the pipe to our previously created group
-			this.pipes.add(badbug);
-
-			// Enable physics on the pipe 
-			game.physics.arcade.enable(badbug);
-
-			// Add velocity to the pipe to make it move left
-			badbug.body.velocity.x = -200;
-
-			// Automatically kill the pipe when it's no longer visible 
-			badbug.checkWorldBounds = true;
-			badbug.outOfBoundsKill = true;
-		},
-
-		addRowOfPipes: function () {
-			// Randomly pick the number between 0 and 7
-			let partition_ht = Math.floor(windowY / 8);
-
-			let randombug = Math.floor(Math.random() * 7);
-			this.addOnePipe(windowX, randombug * partition_ht + 10);
-
-		},
-
-
-		// add food to the game
-		addOnefood: function (x, y) {
-			// Create a pipe at the position x and y
-			let goodbug = game.add.sprite(x, y, 'goodbug');
-			goodbug.scale.setTo(0.1, 0.1);
-			// Add the pipe to our previously created group
-			this.food.add(goodbug);
-
-			// Enable physics on the pipe 
-			game.physics.arcade.enable(goodbug);
-
-			// Add velocity to the pipe to make it move left
-			goodbug.body.velocity.x = -200;
-
-			// Automatically kill the pipe when it's no longer visible 
-			goodbug.checkWorldBounds = true;
-			goodbug.outOfBoundsKill = true;
-		},
-
-		addfood: function () {
-			// Randomly pick a number between 0 and 8
-			// This will be the fud position
-			let part_ht = Math.floor(windowY / 10);
-
-			let goodbugY = Math.floor(Math.random() * 9);
-
-			// With food at position fudY
-			this.addOnefood(windowX, goodbugY * part_ht + 15);
-
-		},
-
-		eatFood: function (bird, goodbug) {
-			this.score += 1;
-			this.labelScore.text = this.score;
-			goodbug.destroy();
-		},
-
-		// Restart the game
-		restartGame: function () {
-			// Start the 'main' state, which restarts the game
-			game.state.start('main');
-		},
-	};
-
-	// Initialize Phaser, and create a 400px by 490px game
-	let game = new Phaser.Game(windowX, windowY, Phaser.AUTO, 'game-area');
-
-	// Add the 'mainState' and call it 'main'
-	game.state.add('main', mainState);
-
-	// Start the state to actually start the game
-	game.state.start('main');
-
-	// let canvas = document.getElementById('circles');
-	// let c = canvas.getContext('2d');
-
-	// for(let i =0; i < 50; i++){
-	// 	let x = Math.random() * window.innerWidth;
-	// 	let y = Math.random() * window.innerHeight;
-	// 	c.beginPath(); 
-	// 	c.arc(x, y, 30, 0, Math.PI * 2, false);
-	// 	c.strokeStyle = 'blue';
-	// 	c.stroke();
-	// }
-
-
-	// Create a new instance of an audio object and adjust some of its properties
-	let audio = document.getElementById('audio_box');
-
-	// Establish all variables that your Analyser will use
-	let canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_y, bar_width, bar_height;
-	// Initialize the MP3 player after the page loads all of its HTML into the window
-	window.addEventListener("load", initMp3Player, false);
-	function initMp3Player() {
-
-		context = new AudioContext(); // AudioContext object instance
-		analyser = context.createAnalyser(); // AnalyserNode method
-		canvas = document.getElementById('analyser_render');
-		ctx = canvas.getContext('2d');
-		// Re-route audio playback into the processing graph of the AudioContext
-		source = context.createMediaElementSource(audio);
-		source.connect(analyser);
-		analyser.connect(context.destination);
-		frameLooper();
-	}
-	// frameLooper() animates any style of graphics you wish to the audio frequency
-	// Looping at the default frame rate that the browser provides(approx. 60 FPS)
-	function frameLooper() {
-		window.requestAnimationFrame(frameLooper);
-		fbc_array = new Uint8Array(analyser.frequencyBinCount);
-		analyser.getByteFrequencyData(fbc_array);
-		ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-		ctx.fillStyle = '#00CCFF'; // Color of the bars
-		bars = 50;
-		for (let i = 0; i < bars; i++) {
-			bar_x = i * 30;
-			bar_width = 25;
-			bar_height = -(fbc_array[i] / 1.25);
-			bar_y = canvas.height;
-			// else if (i > 2) {
-			// 	bar_height = (fbc_array[i - 2] / 1.25);
-			// 	bar_y = 0;
-			// }
-			//fillRect(x, y, width, height) // Explanation of the parameters below
-			ctx.fillRect(bar_x, bar_y, bar_width, bar_height);
-
-		}
-	}
-
+    let X = window.innerWidth;
+    let Y = window.innerHeight;
+    let windowX = Math.ceil(X / 1.5);
+    let windowY = Math.ceil(Y / 1.5) - 30;
+    let gm = document.getElementById('analyser_render');
+    gm.width = X / 1.5;
+    gm.height = Y / 3 - 20;
+    let totalScore = 0;
+    let screen_velocity = 100;
+    let mainState = {
+        preload: function () {
+            // This function will be executed at the beginning     
+            // That's where we load the images and sounds 
+            game.load.image('bird', 'assets/new_bird1.png');
+            game.load.image('goodbug', 'assets/good_bug.png');
+            game.load.image('badbug', 'assets/badbug.png');
+        },
+        create: function () {
+            // This function is called after the preload function     
+            // Here we set up the game, display sprites, etc.  
+            // Change the background color of the game to blue
+            game.stage.backgroundColor = '#002D3C';
+            // Set the physics system
+            game.physics.startSystem(Phaser.Physics.ARCADE);
+            // Display the bird at the position x=100 and y=245
+            this.bird = game.add.sprite(10, windowY / 2, 'bird');
+            this.bird.scale.setTo(0.1, 0.1);
+            // let fly = this.bird.animations.add('fly');
+            // this.bird.animations.play('fly', 3 ,true);
+            // Add physics to the bird
+            // Needed for: movements, gravity, collisions, etc.
+            game.physics.arcade.enable(this.bird);
+            // Add gravity to the bird to make it fall
+            this.bird.body.gravity.y = windowY / 2;
+            // add the pipes group
+            this.pipes = game.add.group();
+            this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+            // add food to the game
+            this.food = game.add.group();
+            this.timer = game.time.events.loop(1000, this.addfood, this);
+            // add the score to the top left 
+            this.score = 0;
+            this.labelScore = game.add.text(20, 20, "0",
+                { font: "30px Arial", fill: "#ffffff" });
+            // Call the 'jump' function when the spacekey is hit
+            let spaceKey = game.input.keyboard.addKey(
+                Phaser.Keyboard.SPACEBAR);
+            spaceKey.onDown.add(this.jump, this);
+        },
+        update: function () {
+            // This function is called 60 times per second    
+            // It contains the game's logic
+            if (this.bird.y < 0 || this.bird.y > windowY)
+                this.restartGame();
+            game.physics.arcade.overlap(
+                this.bird, this.food, this.eatFood, null, this);
+            game.physics.arcade.overlap(
+                this.bird, this.pipes, this.restartGame, null, this);
+        },
+        // Make the bird jump 
+        jump: function () {
+            // Add a vertical velocity to the bird
+            this.bird.body.velocity.y = -windowY / 2;
+        },
+        // add a pipe
+        addOnePipe: function (x, y) {
+            // Create a pipe at the position x and y
+            let badbug = game.add.sprite(x, y, 'badbug');
+            badbug.scale.setTo(0.8, 0.8);
+            // Add the pipe to our previously created group
+            this.pipes.add(badbug);
+            // Enable physics on the pipe 
+            game.physics.arcade.enable(badbug);
+            // Add velocity to the pipe to make it move left
+            badbug.body.velocity.x = -200;
+            // Automatically kill the pipe when it's no longer visible 
+            badbug.checkWorldBounds = true;
+            badbug.outOfBoundsKill = true;
+        },
+        addRowOfPipes: function () {
+            // Randomly pick the number between 0 and 7
+            let partition_ht = Math.floor(windowY / 8);
+            let randombug = Math.floor(Math.random() * 7);
+            this.addOnePipe(windowX, randombug * partition_ht + 10);
+        },
+        // add food to the game
+        addOnefood: function (x, y) {
+            // Create a pipe at the position x and y
+            let goodbug = game.add.sprite(x, y, 'goodbug');
+            goodbug.scale.setTo(0.1, 0.1);
+            // Add the pipe to our previously created group
+            this.food.add(goodbug);
+            // Enable physics on the pipe 
+            game.physics.arcade.enable(goodbug);
+            // Add velocity to the pipe to make it move left
+            //goodbug.body.velocity.x = -200;
+            goodbug.body.velocity.x = -2 * screen_velocity;
+            // Automatically kill the pipe when it's no longer visible 
+            goodbug.checkWorldBounds = true;
+            goodbug.outOfBoundsKill = true;
+        },
+        addfood: function () {
+            // Randomly pick a number between 0 and 8
+            // This will be the fud position
+            let part_ht = Math.floor(windowY / 10);
+            let goodbugY = Math.floor(Math.random() * 9);
+            // With food at position fudY
+            this.addOnefood(windowX, goodbugY * part_ht + 15);
+        },
+        eatFood: function (bird, goodbug) {
+            this.score += 1;
+            totalScore += 1;
+            console.log(totalScore);
+            this.labelScore.text = this.score;
+            goodbug.destroy();
+        },
+        // Restart the game
+        restartGame: function () {
+            // Start the 'main' state, which restarts the game
+            game.state.start('main');
+        },
+    };
+    // Initialize Phaser, and create a 400px by 490px game
+    let game = new Phaser.Game(windowX, windowY, Phaser.AUTO, 'game-area');
+    // Add the 'mainState' and call it 'main'
+    game.state.add('main', mainState);
+    // Start the state to actually start the game
+    game.state.start('main');
+    
+    
+    // Create a new instance of an audio object and adjust some of its properties
+    let audio = document.getElementById('audio_box');
+    audio.src = window.location.search.split("=")[1];
+    // Establish all variables that your Analyser will use
+    let canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_y, bar_width, bar_height;
+    // Initialize the MP3 player after the page loads all of its HTML into the window
+    window.addEventListener("load", initMp3Player, false);
+    function initMp3Player() {
+        context = new AudioContext(); // AudioContext object instance
+        analyser = context.createAnalyser(); // AnalyserNode method
+        canvas = document.getElementById('analyser_render');
+        ctx = canvas.getContext('2d');
+        // Re-route audio playback into the processing graph of the AudioContext
+        source = context.createMediaElementSource(audio);
+        source.connect(analyser);
+        analyser.connect(context.destination);
+        audio.onended = function() {
+            window.open(`page4.html?score=${totalScore}`);
+        };
+        frameLooper();
+    }
+    // frameLooper() animates any style of graphics you wish to the audio frequency
+    // Looping at the default frame rate that the browser provides(approx. 60 FPS)
+    function frameLooper() {
+        window.requestAnimationFrame(frameLooper);
+        fbc_array = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(fbc_array);
+        ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+        ctx.fillStyle = '#00CCFF'; // Color of the bars
+        screen_velocity = fbc_array[0];
+        bars = 50;
+        for (let i = 0; i < bars; i++) {
+            bar_x = i * 30;
+            bar_width = 25;
+            bar_height = -(fbc_array[i] / 1.25);
+            bar_y = canvas.height;
+            
+            //fillRect(x, y, width, height) // Explanation of the parameters below
+            ctx.fillRect(bar_x, bar_y, bar_width, bar_height);
+        }
+    }
 })
