2795776460d67fd66c1d211882455c4516bc5c48
Blythe Totsch <uncblythe1@gmail.com>
2018-08-24 06:50:32 -0500

    refactored page 1 and 2

--- page1.js
+++ page1.js
@@ -1,202 +1,175 @@
 document.addEventListener('DOMContentLoaded', (ev) => {
 
-let selected;
-let basicTimeline = anime.timeline();
-basicTimeline
- .add({
-   targets: '#flappybird',
-   translateY: [0, 500],
-   translateX: [0, 100],
-   loop: false,
-   duration: 2000,
-   rotate: '1turn',
-   easing: 'easeInOutQuad',
- })
- .add({
-   targets: '#flappybird2',
-   translateX: [-5, 400],
-   translateY: 40,
-   opacity: [0, 100],
-   scale: 1.5,
-   loop: false,
-   duration: 3000,
-   easing: 'easeInOutQuad',
- });
+    let selected;
+    let basicTimeline = anime.timeline();
+    basicTimeline
+        .add({
+            targets: '#flappybird',
+            translateY: [0, 500],
+            translateX: [0, 100],
+            loop: false,
+            duration: 2000,
+            rotate: '1turn',
+            easing: 'easeInOutQuad',
+        })
+        .add({
+            targets: '#flappybird2',
+            translateX: [-5, 400],
+            translateY: 40,
+            opacity: [0, 100],
+            scale: 1.5,
+            loop: false,
+            duration: 3000,
+            easing: 'easeInOutQuad',
+        });
 
-function removeChooseSong () {
-    anime ({
-        targets: '.chooseSong',
-        opacity: 0,
-    })
-};
+    function hideChooseSong() {
+        anime({
+            targets: '.chooseSong',
+            opacity: 0,
+        })
+    };
 
-let song1 = document.getElementsByClassName('song1')[0];
-song1.innerText = 'Wrecking Ball';
-song1.addEventListener('click', (ev) => {
-    selected = 'assets/test.mp3';
-    anime ({
-        targets: '.song1',
-        translateX: 36.9,
-        translateY: 20,
-        scale: 2.5,
-        opacity: 1,
-        rotate: '2turn',
+    let song1 = document.getElementsByClassName('song1')[0];
+    song1.innerText = 'Wrecking Ball';
+    song1.addEventListener('click', (ev) => {
+        selected = 'assets/test.mp3';
+        anime({
+            targets: '.song1',
+            translateX: 36.9,
+            translateY: 20,
+            scale: 2.5,
+            opacity: 1,
+            rotate: '2turn',
+        })
+        hideChooseSong();
     })
-    anime ({
-        targets: '.chooseSong',
-        opacity: 0,
-    })
-})
-let song2 = document.getElementsByClassName('song2')[0];
-song2.innerText = 'She Wolf';
-song2.addEventListener('click', (ev) => {
-    selected = 'assets/David Guetta - She Wolf ft. Sia.mp3';  
-    anime ({
-        targets: '.song2',
-        translateX: 30,
-        translateY: 20,
-        scale: 2.5,
-        rotate: '2turn',
-    })
-    anime ({
-        targets: '.chooseSong',
-        opacity: 0,
-    })
-})
-let song3 = document.getElementsByClassName('song3')[0];
-song3.innerText = 'The Monster';
-song3.addEventListener('click', (ev) => {
-    selected = 'assets/12 The Monster (feat. Rihanna).mp3';
-    anime ({
-        targets: '.song3',
-        translateX: 24,
-        translateY: 20,
-        scale: 2.5,
-        rotate: '2turn',
-    })
-    anime ({
-        targets: '.chooseSong',
-        opacity: 0,
-    })
-})
-let song4 = document.getElementsByClassName('song4')[0];
-song4.innerText = 'Lean On';
-song4.addEventListener('click', (ev) => {
-    selected = 'assets/01 Lean On.mp3';
-    anime ({
-        targets: '.song4',
-        translateX: 17,
-        translateY: 20,
-        scale: 2.5,
-        rotate: '2turn',
-    })
-    anime ({
-        targets: '.chooseSong',
-        opacity: 0,
-    })
-})
 
-let song5 = document.getElementsByClassName('song5')[0];
-song5.innerText = 'Young Blood';
-song5.addEventListener('click', (ev) => {
-    selected = 'assets/01 Young Blood.m4a';
-    anime ({
-        targets: '.song5',
-        translateX: 10,
-        translateY: 20,
-        scale: 2.5,
-        rotate: '2turn',
+    let song2 = document.getElementsByClassName('song2')[0];
+    song2.innerText = 'She Wolf';
+    song2.addEventListener('click', (ev) => {
+        selected = 'assets/David Guetta - She Wolf ft. Sia.mp3';
+        anime({
+            targets: '.song2',
+            translateX: 30,
+            translateY: 20,
+            scale: 2.5,
+            rotate: '2turn',
+        })
+        hideChooseSong();
     })
-    anime ({
-        targets: '.chooseSong',
-        opacity: 0,
-    })
-})
 
-let song6 = document.getElementsByClassName('song6')[0];
-song6.innerText = 'Work From Home';
-song6.addEventListener('click', (ev) => {
-    selected = 'assets/Fifth Harmony - Work from Home ft. Ty Dolla $ign.mp3';
-    anime ({
-        targets: '.song6',
-        translateX: 4,
-        translateY: 20,
-        scale: 2.5,
-        rotate: '2turn',
+    let song3 = document.getElementsByClassName('song3')[0];
+    song3.innerText = 'The Monster';
+    song3.addEventListener('click', (ev) => {
+        selected = 'assets/12 The Monster (feat. Rihanna).mp3';
+        anime({
+            targets: '.song3',
+            translateX: 24,
+            translateY: 20,
+            scale: 2.5,
+            rotate: '2turn',
+        })
+        hideChooseSong();
     })
-    anime ({
-        targets: '.chooseSong',
-        opacity: 0,
-    })
-})
 
-let song7 = document.getElementsByClassName('song7')[0];
-song7.innerText = `Don't Let Me Down`;
-song7.addEventListener('click', (ev) => {
-    selected = `assets/The Chainsmokers - Don't Let Me Down (Audio) ft. Daya.mp3`;
-    anime ({
-        targets: '.song7',
-        translateX: -4,
-        translateY: 20,
-        scale: 2.5,
-        rotate: '2turn',
+    let song4 = document.getElementsByClassName('song4')[0];
+    song4.innerText = 'Lean On';
+    song4.addEventListener('click', (ev) => {
+        selected = 'assets/01 Lean On.mp3';
+        anime({
+            targets: '.song4',
+            translateX: 17,
+            translateY: 20,
+            scale: 2.5,
+            rotate: '2turn',
+        })
+        hideChooseSong();
     })
-    anime ({
-        targets: '.chooseSong',
-        opacity: 0,
-    })
-})
 
-let song8 = document.getElementsByClassName('song8')[0];
-song8.innerText = 'Chandelier';
-song8.addEventListener('click', (ev) => {
-    selected = 'assets/Chandelier.mp3';
-    anime ({
-        targets: '.song8',
-        translateX: -10,
-        translateY: 20,
-        scale: 2.5,
-        rotate: '2turn',
+    let song5 = document.getElementsByClassName('song5')[0];
+    song5.innerText = 'Young Blood';
+    song5.addEventListener('click', (ev) => {
+        selected = 'assets/01 Young Blood.m4a';
+        anime({
+            targets: '.song5',
+            translateX: 10,
+            translateY: 20,
+            scale: 2.5,
+            rotate: '2turn',
+        })
+        hideChooseSong();
     })
-    anime ({
-        targets: '.chooseSong',
-        opacity: 0,
-    })
-})
 
-let song9 = document.getElementsByClassName('song9')[0];
-song9.innerText = 'Head Full of Dreams';
-song9.addEventListener('click', (ev) => {
-    selected = 'assets/11 Up & Up.mp3';
-    anime ({
-        targets: '.song9',
-        translateX: -15,
-        translateY: 20,
-        scale: 2.5,
-        rotate: '2turn',
+    let song6 = document.getElementsByClassName('song6')[0];
+    song6.innerText = 'Work From Home';
+    song6.addEventListener('click', (ev) => {
+        selected = 'assets/Fifth Harmony - Work from Home ft. Ty Dolla $ign.mp3';
+        anime({
+            targets: '.song6',
+            translateX: 4,
+            translateY: 20,
+            scale: 2.5,
+            rotate: '2turn',
+        })
+        hideChooseSong();
     })
-    anime ({
-        targets: '.chooseSong',
-        opacity: 0,
-    })
-})
 
-let song10 = document.getElementsByClassName('song10')[0];
-song10.innerText = '1989';
-song10.addEventListener('click', (ev) => {
-    selected = 'assets/02 Blank Space.mp3';
-    anime ({
-        targets: '.song10',
-        translateX: -22,
-        translateY: 20,
-        scale: 2.5,
-        rotate: '2turn',
+    let song7 = document.getElementsByClassName('song7')[0];
+    song7.innerText = `Don't Let Me Down`;
+    song7.addEventListener('click', (ev) => {
+        selected = `assets/The Chainsmokers - Don't Let Me Down (Audio) ft. Daya.mp3`;
+        anime({
+            targets: '.song7',
+            translateX: -4,
+            translateY: 20,
+            scale: 2.5,
+            rotate: '2turn',
+        })
+        hideChooseSong();
     })
-    anime ({
-        targets: '.chooseSong',
-        opacity: 0,
+
+    let song8 = document.getElementsByClassName('song8')[0];
+    song8.innerText = 'Chandelier';
+    song8.addEventListener('click', (ev) => {
+        selected = 'assets/Chandelier.mp3';
+        anime({
+            targets: '.song8',
+            translateX: -10,
+            translateY: 20,
+            scale: 2.5,
+            rotate: '2turn',
+        })
+        hideChooseSong();
     })
-})
+
+    let song9 = document.getElementsByClassName('song9')[0];
+    song9.innerText = 'Head Full of Dreams';
+    song9.addEventListener('click', (ev) => {
+        selected = 'assets/11 Up & Up.mp3';
+        anime({
+            targets: '.song9',
+            translateX: -15,
+            translateY: 20,
+            scale: 2.5,
+            rotate: '2turn',
+        })
+        hideChooseSong();
+    })
+
+    let song10 = document.getElementsByClassName('song10')[0];
+    song10.innerText = '1989';
+    song10.addEventListener('click', (ev) => {
+        selected = 'assets/02 Blank Space.mp3';
+        anime({
+            targets: '.song10',
+            translateX: -22,
+            translateY: 20,
+            scale: 2.5,
+            rotate: '2turn',
+        })
+        hideChooseSong();
+    })
 
     let animateTitle = anime({
         targets: 'h1',
@@ -218,7 +191,8 @@ song10.addEventListener('click', (ev) => {
         delay: 100,
         duration: 1000
     })
-    let animateSong1 = anime ({
+
+    let animateSong1 = anime({
         targets: '.song1',
         translateX: '15rem',
         translateY: '28rem',
@@ -230,7 +204,8 @@ song10.addEventListener('click', (ev) => {
         loop: false,
         duration: 1000
     })
-    let animateSong2 = anime ({
+
+    let animateSong2 = anime({
         targets: '.song2',
         translateX: '20rem',
         translateY: '34rem',
@@ -242,7 +217,8 @@ song10.addEventListener('click', (ev) => {
         loop: false,
         duration: 1000
     })
-    let animateSong3 = anime ({
+
+    let animateSong3 = anime({
         targets: '.song3',
         translateX: '25rem',
         translateY: '36rem',
@@ -254,7 +230,8 @@ song10.addEventListener('click', (ev) => {
         loop: false,
         duration: 1000
     })
-    let animateSong4 = anime ({
+
+    let animateSong4 = anime({
         targets: '.song4',
         translateX: '30rem',
         translateY: '33rem',
@@ -266,7 +243,8 @@ song10.addEventListener('click', (ev) => {
         loop: false,
         duration: 1000
     })
-    let animateSong5 = anime ({
+
+    let animateSong5 = anime({
         targets: '.song5',
         translateX: '33rem',
         translateY: '23rem',
@@ -278,7 +256,8 @@ song10.addEventListener('click', (ev) => {
         loop: false,
         duration: 1000
     })
-    let animateSong6 = anime ({
+
+    let animateSong6 = anime({
         targets: '.song6',
         translateX: '25rem',
         translateY: '12rem',
@@ -290,7 +269,8 @@ song10.addEventListener('click', (ev) => {
         loop: false,
         duration: 1000
     })
-    let animateSong7 = anime ({
+
+    let animateSong7 = anime({
         targets: '.song7',
         translateX: '9rem',
         translateY: '5rem',
@@ -302,7 +282,8 @@ song10.addEventListener('click', (ev) => {
         loop: false,
         duration: 1000
     })
-    let animateSong8 = anime ({
+
+    let animateSong8 = anime({
         targets: '.song8',
         translateX: '-10rem',
         translateY: '3rem',
@@ -314,7 +295,8 @@ song10.addEventListener('click', (ev) => {
         loop: false,
         duration: 1000
     })
-    let animateSong9 = anime ({
+
+    let animateSong9 = anime({
         targets: '.song9',
         translateX: '-28rem',
         translateY: '5rem',
@@ -326,7 +308,8 @@ song10.addEventListener('click', (ev) => {
         loop: false,
         duration: 1000
     })
-    let animateSong10 = anime ({
+
+    let animateSong10 = anime({
         targets: '.song10',
         translateX: '-45rem',
         translateY: '12rem',
@@ -338,69 +321,80 @@ song10.addEventListener('click', (ev) => {
         loop: false,
         duration: 1000
     })
-    let animateInstructions = anime ({
+
+    let animateInstructions = anime({
         targets: '#instructions',
-        translateY : '10rem',
-        translateX : '-5rem',
+        translateY: '10rem',
+        translateX: '-5rem',
         opacity: [0, .9],
         delay: 1100,
         duration: 1000,
     })
-    let animatePlaySong = anime ({
+
+    let animatePlaySong = anime({
         targets: '#playSong',
-        translateY : '25rem',
-        translateX : '5rem',
+        translateY: '25rem',
+        translateX: '5rem',
         opacity: [0, .9],
         delay: 1100,
         duration: 1000,
     })
-    let animatePlayGame = anime ({
+
+    let animatePlayGame = anime({
         targets: '#playGame',
-        translateY : '30rem',
-        translateX : '8rem',
+        translateY: '30rem',
+        translateX: '8rem',
         opacity: [0, .9],
         delay: 1100,
         duration: 1000,
     })
+
     let instructions = document.getElementById('instructions');
     instructions.addEventListener('click', (ev) => {
         instructions.innerText = '';
+
         let instructionList = document.createElement('ul');
         instructionList.innerText = 'INSTRUCTIONS:'
         instructions.appendChild(instructionList);
+
         let liOne = document.createElement('li');
         instructionList.appendChild(liOne);
         liOne.innerText = "Use the spacebar to fly."
+
         let liTwo = document.createElement('li');
         instructionList.appendChild(liTwo);
         liTwo.innerText = "Collect the pink bugs for your babies."
+
         let liThree = document.createElement('li');
         instructionList.appendChild(liThree);
         liThree.innerText = "Be aware of the poisonous yellow bugs - they will kill you."
-        anime ({
+
+        anime({
             targets: '.chooseSong',
             opacity: 0,
         })
-        anime ({
+        anime({
             targets: 'h1',
             opacity: 0,
         })
-        anime ({
+        anime({
             targets: '.song',
             opacity: 0,
         })
-        anime ({
+        anime({
             targets: '#instructions',
             scale: [1, 4],
-            translateY : '5rem',
-            translateX : '-10rem',
+            translateY: '5rem',
+            translateX: '-10rem',
         })
-        
+
     })
+
     let playSong = document.getElementById('playSong');
     playSong.addEventListener('click', (ev) => {
         window.open(`page3.html?song=${selected}`);
     })
+
     let playGame = document.getElementById('playGame');
     playGame.addEventListener('click', (ev) => {
         window.open(`page2.html?song=${selected}`);
--- page2.js
+++ page2.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
     let X = window.innerWidth;
     let Y = window.innerHeight;
     let windowX = Math.ceil(X / 1.5);
@@ -9,7 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
     gm.height = Y / 3 - 20;
     let totalScore = 0;
     let screen_velocity = 100;
-
     let mainState = {
         preload: function () {
             // This function will be executed at the beginning     
@@ -18,31 +16,24 @@ document.addEventListener('DOMContentLoaded', () => {
             game.load.image('goodbug', 'assets/good_bug.png');
             game.load.image('badbug', 'assets/badbug.png');
         },
-
         create: function () {
             // This function is called after the preload function     
             // Here we set up the game, display sprites, etc.  
             // Change the background color of the game to blue
             game.stage.backgroundColor = '#002D3C';
-
             // Set the physics system
             game.physics.startSystem(Phaser.Physics.ARCADE);
-
             // Display the bird at the position x=100 and y=245
             this.bird = game.add.sprite(10, windowY / 2, 'bird');
             this.bird.scale.setTo(0.1, 0.1);
-
             // Add physics to the bird
             // Needed for: movements, gravity, collisions, etc.
             game.physics.arcade.enable(this.bird);
-
             // Add gravity to the bird to make it fall
             this.bird.body.gravity.y = windowY / 2;
-
-            // add the pipes group
-            this.pipes = game.add.group();
-            this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
-            
+            // add the bad bugs group
+            this.bBugs = game.add.group();
+            this.timer = game.time.events.loop(1500, this.addRowOfBugs, this);
             // add food to the game
             this.food = game.add.group();
             this.timer = game.time.events.loop(1000, this.addfood, this);
@@ -63,47 +54,46 @@ document.addEventListener('DOMContentLoaded', () => {
             game.physics.arcade.overlap(
                 this.bird, this.food, this.eatFood, null, this);
             game.physics.arcade.overlap(
-                this.bird, this.pipes, this.restartGame, null, this);
+                this.bird, this.bBugs, this.restartGame, null, this);
         },
         // Make the bird jump 
         jump: function () {
             // Add a vertical velocity to the bird
             this.bird.body.velocity.y = -windowY / 2;
         },
-        // add a pipe
-        addOnePipe: function (x, y) {
-            // Create a pipe at the position x and y
+        // add a bad bug
+        addOneBug: function (x, y) {
+            // Create a bad bug at the position x and y
             let badbug = game.add.sprite(x, y, 'badbug');
             badbug.scale.setTo(0.8, 0.8);
-            // Add the pipe to our previously created group
-            this.pipes.add(badbug);
-            // Enable physics on the pipe 
+            // Add the bad bug to our previously created group
+            this.bBugs.add(badbug);
+            // Enable physics on the bad bug 
             game.physics.arcade.enable(badbug);
-            // Add velocity to the pipe to make it move left
+            // Add velocity to the bad bug to make it move left
             badbug.body.velocity.x = -200;
-            // Automatically kill the pipe when it's no longer visible 
+            // Automatically kill the bad bug when it's no longer visible 
             badbug.checkWorldBounds = true;
             badbug.outOfBoundsKill = true;
         },
-        addRowOfPipes: function () {
+        addRowOfBugs: function () {
             // Randomly pick the number between 0 and 7
             let partition_ht = Math.floor(windowY / 8);
             let randombug = Math.floor(Math.random() * 7);
-            this.addOnePipe(windowX, randombug * partition_ht + 10);
+            this.addOneBug(windowX, randombug * partition_ht + 10);
         },
         // add food to the game
         addOnefood: function (x, y) {
-            // Create a pipe at the position x and y
+            // Create a bad bug at the position x and y
             let goodbug = game.add.sprite(x, y, 'goodbug');
             goodbug.scale.setTo(0.1, 0.1);
-            // Add the pipe to our previously created group
+            // Add the bad bug to our previously created group
             this.food.add(goodbug);
-            // Enable physics on the pipe 
+            // Enable physics on the bad bug 
             game.physics.arcade.enable(goodbug);
-            // Add velocity to the pipe to make it move left
-            //goodbug.body.velocity.x = -200;
+            // Add velocity to the bad bug to make it move left
             goodbug.body.velocity.x = -2 * screen_velocity;
-            // Automatically kill the pipe when it's no longer visible 
+            // Automatically kill the bad bug when it's no longer visible 
             goodbug.checkWorldBounds = true;
             goodbug.outOfBoundsKill = true;
         },
@@ -134,11 +124,10 @@ document.addEventListener('DOMContentLoaded', () => {
     game.state.add('main', mainState);
     // Start the state to actually start the game
     game.state.start('main');
-    
-    
     // Create a new instance of an audio object and adjust some of its properties
     let audio = document.getElementById('audio_box');
     audio.src = window.location.search.split("=")[1];
+    // audio.width = windowX;
     // Establish all variables that your Analyser will use
     let canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_y, bar_width, bar_height;
     // Initialize the MP3 player after the page loads all of its HTML into the window
@@ -152,7 +141,7 @@ document.addEventListener('DOMContentLoaded', () => {
         source = context.createMediaElementSource(audio);
         source.connect(analyser);
         analyser.connect(context.destination);
-        audio.onended = function() {
+        audio.onended = function () {
             window.open(`page4.html?score=${totalScore}`);
         };
         frameLooper();
@@ -172,7 +161,6 @@ document.addEventListener('DOMContentLoaded', () => {
             bar_width = 25;
             bar_height = -(fbc_array[i] / 1.25);
             bar_y = canvas.height;
-            
             //fillRect(x, y, width, height) // Explanation of the parameters below
             ctx.fillRect(bar_x, bar_y, bar_width, bar_height);
         }
